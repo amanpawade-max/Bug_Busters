bfb9d2ed6a7d3db6b711680be18e437738fb6ddb
shreyyoo-gl <shreyas.chaurey@globallogic.com>
2026-07-29 15:38:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Aggressively limit memory usage for FastEmbed / ONNX Runtime
+export OMP_NUM_THREADS=1
+export MALLOC_ARENA_MAX=2
+export RAY_memory_monitor_refresh_ms=0
+
 # Start the FastAPI backend in the background on port 8000
 uvicorn server:app --host 0.0.0.0 --port 8000 &
 
